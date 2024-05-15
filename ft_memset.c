284f89bca5bbc4abfdf38b79daa271aba8bbc725
Tsiangana Zau <128613939+Tsiangana@users.noreply.github.com>
2024-05-15 16:54:28 +0100

    libft
    testando

---
+++ ft_memset.c
@@ -0,0 +1,30 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_memset.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: pzau <marvin@42.fr>                        +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/05/15 15:50:27 by pzau              #+#    #+#             */
+/*   Updated: 2024/05/15 16:50:46 by pzau             ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+void	*ft_memset(void *s, int c, size_t n)
+{
+	unsigned char	*ptr;
+	unsigned char	value;
+	int				i;
+
+	ptr = (unsigned char *)s;
+	value = (unsigned char)c;
+	i = 0;
+	while (i < n)
+	{
+		ptr[i] = value;
+		i++;
+	}
+	return (s);
+}
